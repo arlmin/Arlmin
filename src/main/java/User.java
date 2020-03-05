8d50cef51897898de20449bdc943e3a91e1b188b
arlmin <bloodanguts@foxmail.com>
2020-03-05 11:57:49 +0800

    a

---
+++ src/main/java/User.java
@@ -0,0 +1,10 @@
+/**
+ * @ClassName:User
+ * @Package:PACKAGE_NAME
+ * @Date 2020/3/5 - 11:47
+ * @Author:bloodandguts@foxmail.com
+ * @Description:
+ */
+public class User {
+    private  String username;
+}
